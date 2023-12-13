45017514a02147ae385fc1f162eaa5711350a3c1
swapnilongithub <65008098+swapnilongithub@users.noreply.github.com>
2023-12-13 14:31:07 +0530

    Updated code

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log("hellow page loaded");
+document.addEventListener("DOMContentLoaded", function () {
+  const filterInput = document.getElementById("filterInput");
+  const listItems = document.querySelectorAll(".list-item");
+
+  filterInput.addEventListener("input", function () {
+    const filterText = filterInput.value.toLowerCase();
+
+    listItems.forEach(function (item) {
+      const itemText = item.textContent.toLowerCase();
+
+      if (itemText.includes(filterText)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+});
+
