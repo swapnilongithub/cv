dead650c7d73d5529509f9b1609d881de5bb4a69
swapnilongithub <65008098+swapnilongithub@users.noreply.github.com>
2023-12-19 16:53:08 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,11 @@
-document.getElementById('app').style.color = 'blue';
+// test.js
+document.addEventListener("DOMContentLoaded", function() {
+    // This function will be executed when the DOM is fully loaded
+
+    // Get the element with the id 'app'
+    var appElement = document.getElementById('app');
+
+    // Change the text color to red
+    appElement.style.color = 'red';
+});
+
